b3cdc10385beb43fca13ee5559cfaedf50d67f35
xlsft <dev@xlsft.ru>
2026-01-13 23:45:18 +0300

    feat: basic inventory

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
-
+    import Inventory from './components/Inventory.vue';
 </script>
 
 <template>
-  
+    <main>
+        <div class="inventories">
+            <Suspense>
+                <Inventory entity="user"/>                
+            </Suspense>
+            <Suspense>
+                <Inventory entity="bot"/>
+            </Suspense>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+    main {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .inventories {
+        display: flex;
+        gap: 12px;
+        flex-grow: 1;
+        @media screen and (width < 768px) {
+            flex-direction: column;
+        }
+    }
+</style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+    import { useStatic } from '../composables/useStatic';
+    import type { Item } from '../types/item.types';
+
+    const props = defineProps<{
+        entity: 'user' | 'bot'
+    }>()
+
+    const data = await useStatic<Item>(`${props.entity}.inventory`)
+</script>
+
+<template>
+    <div class="inventory" :data-entity="props.entity">
+        {{ data }}
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        position: absolute;
+        left: 12px;
+    }
+
+    .inventory {
+        position: relative;
+        flex-grow: 1;
+        display: grid;
+        padding: 12px;
+        border-radius: 12px;
+        border: 1px #EFEFEF solid;
+        grid-template-columns: repeat(4, 1fr);
+    }
+</style>
