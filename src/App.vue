c6fc72a80adb011d0cdbea2b32486c1023e2c3db
xlsft <dev@xlsft.ru>
2026-01-14 01:48:16 +0300

    feat: trade ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
+    import { ref, useTemplateRef } from 'vue';
     import Inventory from './components/Inventory.vue';
+    import type { ItemType } from './types/item.types';
+    import Container from './components/Container.vue';
+    import Item from './components/Item.vue';
+
+    const selected = ref<{ user: ItemType[], bot: ItemType[] }>({ user: [], bot: [] })
+
+    const user = useTemplateRef('user')
+    const bot = useTemplateRef('bot')
 </script>
 
 <template>
     <main>
-        <div class="inventories">
+        <section>
+            <Container :title="`selected: ${selected.user.length}/${user?.max || 0}`" class="selected">
+                <Item :item v-for="item in selected.user" @click="user?.unset(item)"/>
+            </Container>
+            <Container title="selected item" class="selected bot">
+                <Item :item v-for="item in selected.bot" @click="bot?.unset(item)"/>
+            </Container>
+        </section>
+        <section class="inventories">
             <Suspense>
-                <Inventory entity="user"/>                
+                <Inventory v-model="selected.user" multiple entity="user" ref="user"/>                
             </Suspense>
             <Suspense>
-                <Inventory entity="bot"/>
+                <Inventory v-model="selected.bot" entity="bot" ref="bot"/>
             </Suspense>
-        </div>
+        </section>
     </main>
 </template>
 
@@ -20,14 +37,25 @@
         flex-grow: 1;
         display: flex;
         flex-direction: column;
-    }
-
-    .inventories {
-        display: flex;
         gap: 12px;
-        flex-grow: 1;
-        @media screen and (width < 768px) {
-            flex-direction: column;
+        section {
+            display: flex;
+            gap: 12px;
+            @media screen and (width < 768px) { flex-direction: column; }
+            &.inventories {
+                flex-grow: 1;
+            }
+            .selected {
+                height: 100px;
+                flex-direction: row;
+                @media screen and (width > 768px) { &.bot {
+                    max-width: 200px;
+                }}
+                * {
+                    max-height: 100%;
+                }
+            }
         }
+        
     }
 </style>
--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,28 +1,52 @@
 <script setup lang="ts">
     import { useStatic } from '../composables/useStatic';
     import type { ItemType } from '../types/item.types';
+    import Container from './Container.vue';
+    import Item from './Item.vue';
 
-    const props = defineProps<{
-        entity: 'user' | 'bot'
-    }>()
+    const props = withDefaults(defineProps<{ entity: 'user' | 'bot', multiple?: boolean, max?: number }>(), { max: 6 })
+    const selected = defineModel<ItemType[]>({ required: true, default: [] })
 
-    const data = await useStatic<ItemType>(`${props.entity}.inventory`)
+    const data = (await useStatic<ItemType[]>(`${props.entity}.inventory`)).sort((a, b) => a.name.localeCompare(b.name))
+
+    defineExpose({
+        unset: (item: ItemType) => {
+            if (!selected.value.length) return
+            const i = selected.value.indexOf(item)
+            if (i === -1) return
+            selected.value.splice(i, 1)
+        },
+        data: data,
+        max: props.max || data.length
+    })
 </script>
 
 <template>
-    <div class="inventory" :data-entity="props.entity">
-        {{ data }}
-    </div>
+    <Container 
+        :title="`${props.entity} inventory`" 
+        class="inventory" 
+        :data-disabled="
+            selected.length >= Math.min(props.max, data.length) || 
+            (!props.multiple && selected.length === 1) ||
+            !data.filter(v => !selected.find(s => v.id === s.id)).length
+        "
+    >
+        <Item :item v-for="item in data.filter(v => !selected.find(s => v.id === s.id))" @click="selected.push(item)"/>
+    </Container>
 </template>
 
 <style scoped>
     .inventory {
-        position: relative;
-        flex-grow: 1;
+        width: 100%;
+        max-height: calc(100dvh - 100px - 48px - 12px);
         display: grid;
-        padding: 12px;
-        border-radius: 12px;
-        border: 1px #EFEFEF solid;
-        grid-template-columns: repeat(4, 1fr);
+        --cols: 4; grid-template-columns: repeat(var(--cols), 1fr);
+        grid-auto-flow: row dense;
+        grid-auto-rows: auto;
+        align-content: start;
+        &[data-disabled="true"] { *, & {
+            opacity: .4;
+            pointer-events: none;
+        }}
     }
 </style>
