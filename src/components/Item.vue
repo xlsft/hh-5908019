934e6c33ac5234b53c26a7724e983cb2f3bd4299
xlsft <dev@xlsft.ru>
2026-01-14 01:47:37 +0300

    feat: `Item` component

---
+++ src/components/Item.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+    import { computed } from 'vue';
+    import type { ItemType } from '../types/item.types';
+    import Container from './Container.vue';
+
+    const props = defineProps<{ item: ItemType }>()
+
+    const title = computed(() => {
+        const [i0, i1] = props.item.name.split(' ')
+        return `${(i0 || '')[0]}${(i1 || '')[0]}`
+    })
+</script>
+
+<template>
+    <Container class="item">
+        <h3>{{ title }}</h3>
+        <span>{{ props.item.name }}</span>
+    </Container>
+</template>
+
+<style scoped>
+    h3 { 
+        margin: 0; 
+        line-height: 10px;
+    }
+    .item {
+        width: calc(
+            (100cqi - (var(--gap) * (var(--cols) - 1)))
+            / var(--cols)
+        );
+        height: calc(
+            (100cqi - (var(--gap) * (var(--cols) - 1)))
+            / var(--cols)
+        );
+        aspect-ratio: 1 / 1;
+        cursor: pointer;
+    }
+
+    .item:hover {
+        border-color: red;
+    }
+</style>
